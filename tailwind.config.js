2e708ee0087ef13dea77004cda5580cf7d2687aa
AbdullohZokirov <abdullazokirov808@gmail.com>
2024-12-21 11:01:24 +0500

    18-dars

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        text: "#262626",
+      },
+      borders: {
+        border: "7px",
+      },
+    },
+  },
+  plugins: [],
+};
